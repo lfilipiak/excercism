cd38116cebee3be99aaa21eaeac9c323d8ddedcf
lfilipiak <lukaszfilipiak1@gmail.com>
2022-08-07 22:45:04 +0200

    5 exc

---
+++ Etl.py
@@ -0,0 +1,15 @@
+def transform(legacy_data):
+    list = []
+    value = {}
+    
+    for x in legacy_data.values():
+        list.extend(x)
+    
+    list.sort()
+    
+    for element in list:
+        x = []
+        x = [k for k, v in legacy_data.items() if element in v]
+        value.update({element.lower():x[0]})
+    
+    return value
