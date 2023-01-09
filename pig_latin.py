8bb61f2e7bd738965858740913efdc6c29031cca
lfilipiak <lukaszfilipiak1@gmail.com>
2023-01-10 00:10:50 +0100

    pigs

---
+++ pig_latin.py
@@ -0,0 +1,29 @@
+def _rotate(word):
+
+    return word[1:] + word[0]
+
+
+def _pig_latin(word):
+
+    if word[:2] == "xr":
+
+        return "xrayay"  # for some reason
+
+    if word[0] == "y" and word[1] in "aeiou":
+
+        word = _rotate(word)
+
+    while word[0] not in "aeiouy":
+
+        word = _rotate(word)
+
+    if word[-1] == "q" and word[0] == "u":
+
+        word = _rotate(word)
+
+    return word + "ay"
+
+
+def translate(text):
+
+    return " ".join([_pig_latin(word) for word in text.split()])
