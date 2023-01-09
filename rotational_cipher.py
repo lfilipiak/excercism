669078b1b37b7774f87637010d417c6cf19c3d5a
lfilipiak <lukaszfilipiak1@gmail.com>
2023-01-09 20:21:48 +0100

    cesar cipher

--- rotational_cipher.py
+++ rotational_cipher.py
@@ -2,14 +2,29 @@ def rotate(text: str, key: int) -> str:
     sentence = ""
 
     for char in text:
-        if 65 > ord(char) > 90 or 97 > ord(char) < 122:
+
+        if (ord(char) >= 65 and ord(char) <= 90) or (
+            ord(char) >= 97 and ord(char) <= 122
+        ):
+            if ord(char) >= 97:
+                if ord(char) + key % 26 not in range(97, 123, 1):
+                    sentence = sentence + "".join(
+                        chr(ord(char) + key % 26 - ord("z") + ord("a") - 1)
+                    )
+                else:
+                    sentence = sentence + "".join(chr(ord(char) + key % 26))
+            else:
+                if ord(char) + key % 26 not in range(65, 91, 1):
+                    sentence = sentence + "".join(
+                        chr(ord(char) + key % 26 - ord("Z") + ord("A") - 1)
+                    )
+                else:
+                    sentence = sentence + "".join(chr(ord(char) + key % 26))
+        else:
             sentence = sentence + char
             continue
-        else:
-            sentence = sentence + "".join(chr(ord(char) + key % 26))
-        # sentence = sentence + "".join(chr(ord(char) + key % 26))
 
     return sentence
 
 
-print(rotate("ASD", 10))
+print(rotate("ABC BCA ZZZ AAA zzz aaa '''", 36))
