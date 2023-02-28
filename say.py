61d2abbdc927e0a1ecd360c5255edda93a6e6306
lfilipiak <lukaszfilipiak1@gmail.com>
2023-03-01 00:31:53 +0100

    while if flags

--- say.py
+++ say.py
@@ -1,8 +1,16 @@
+import logging as log
+
+log.basicConfig(level=log.DEBUG, format="%(message)s")
+
+
 def say(number):
 
     sentence = ""
-    list_of_ints = [x for x in str(number)]
-    len_of_number = len(list_of_ints)
+    tens_flag = True
+    hundreds_flag = True
+    list_of_digits = [x for x in str(number)]
+    len_of_number = len(list_of_digits)
+    chunks_value = (len_of_number - 1) // 3
 
     if number < 0 or number > 999_999_999_999:
         raise ValueError("input out of range")
@@ -31,18 +39,51 @@ def say(number):
         1_000_000_000: "billion",
     }
 
-    if number <= 13:
-        return translate[number]
-    elif number >= 14 and number <= 19 and number != 15 and number != 18:
-        return f"{translate[number-10]}teen"
-    elif number == 15:
-        return "fifteen"
-    elif number == 18:
-        return "eighteen"
+    # chunks = {
+    #     0: "hundred",
+    #     1: "thousand",
+    #     2: "million",
+    #     3: "billion",
+    # }
 
-    if len_of_number >= 10:
-        sentence = f"{translate[int(list_of_ints[0])]}"
+    list_of_digits.reverse()
+
+    while list_of_digits:
+        if tens_flag:
+            x = "".join(list_of_digits[0:2])
+            if number <= 13:
+                sentence +=
+                # return translate[number]
+            elif number >= 14 and number <= 19 and number != 15 and number != 18:
+                # return f"{translate[number-10]}teen"
+            elif number == 15:
+                # return "fifteen"
+            elif number == 18:
+                # return "eighteen"
+            elif number in translate.keys():
+                # return translate[number]
+            tens_flag = False
+            list_of_digits.pop(0)
+            list_of_digits.pop(0)
+            log.info(list_of_digits)
+            log.info(x)
+        if hundreds_flag:
+
+    
+    log.info(len_of_number)
+
     return sentence
 
+    
 
-print(say(2233123123))
+    # if number >= 21:
+    #     for digit in list_of_digits:
+    #         if len_of_number < 3:
+    #             sentence += f"{translate[int(list_of_digits[0])]}"
+
+    #         log.info(chunks[chunks_value])
+    #         log.info(sentence)
+    # return sentence
+
+
+print(say(123456789012))
